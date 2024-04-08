63f7c7d8df6ff3e8c8a6156f510c1c351d44b188
juan1031 <jdbv2000@gmail.com>
2024-04-08 18:56:56 -0500

    update

--- navigation/Punto_2.py
+++ navigation/Punto_2.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
+file1 = open('./data/punto_2/pregunta.md').read()
+
 
 def punto_dos():
 
     # -------------------------------------------------------------
-    st.title("Punto 2")
+    st.title("Punto 2 ")
     # -------------------------------------------------------------
 
-    st.markdown(
-        '''
-        ''')
+    st.markdown(file1)
