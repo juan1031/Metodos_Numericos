a4b953b33fbc1a0fd2a6d43a88394e2e1fa11b94
mariajosecastillo222 <mariajosecastillo172461@gmail.com>
2024-04-08 17:04:03 -0500

    Punto 1 avances

--- navigation/Punto_1.py
+++ navigation/Punto_1.py
@@ -31,9 +31,33 @@ def punto_uno():
     # -------------------------------------------------------------
     st.title("Punto 1")
     # -------------------------------------------------------------
+    st.subheader("Ecuaciones de Valor")
+    st.write("### ¿Qué son las Ecuaciones de Valor?")
+    st.write("Las ecuaciones de valor son herramientas fundamentales en el campo de las finanzas que nos permiten analizar y comparar flujos de efectivo en diferentes momentos del tiempo. En esta aplicación, nos centraremos en comprender su significado y cómo podemos resolverlas utilizando Python.")
+
+    st.write("### ¿Para qué sirven las Ecuaciones de Valor?")
+    st.write("Las ecuaciones de valor son útiles para una variedad de propósitos en el mundo financiero, desde la valoración de inversiones hasta la toma de decisiones de financiamiento. Aquí exploraremos algunos de los casos de uso más comunes.")
+
+    st.write("### Objetivo")
+    st.write("Construir una clase en Python que permita solucionar numéricamente las ecuaciones de valor.")
+
+    st.write("### Problema")
+    st.write("Ecuación de valor. Cuando se quiere hallar un flujo de caja equivalente se utiliza una ecuación de valor y así se construye una condición (ecuación) que permite hallar el flujo equivalente. En algunos casos, hallar la solución a dicha condición no es una tarea sencilla. Tenga en cuenta que en algunos casos la variable de interés no es necesariamente el flujo de caja. Otras variables de interés pueden ser la periodicidad o una tasa de interés.")
+    st.write("Reto: Construir una clase que reciba un argumento, (tasa, flujo o n). Si recibe")
+    st.markdown("- **tasa**: entonces halle la tasa que cumple la ecuación de valor.")
+    st.markdown("- **flujo**: entonces halle el flujo que cumple la ecuación de valor.")
+    st.markdown("- **n**: entonces halle el valor n que cumple la ecuación de valor.")
+
+    st.markdown("Por ende la clase construida es:")
+
+
+    #clase 
 
     st.markdown('##')
     st.divider()
+
+    st.markdown("Con ayuda de la clase construida, responder a los siguientes problemas: ")
+
     page = hc.option_bar(
         option_definition=option_data,
         title='Subsecciones',
