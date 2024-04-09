65824549540dc2810ad32bd15462992cb9e9a68e
juan1031 <jdbv2000@gmail.com>
2024-04-09 08:27:11 -0500

    update

--- navigation/Punto_2.py
+++ navigation/Punto_2.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+from src.MetodosNumericos import MetodosNumericos
+from src.funciones import tiempo_vs_precision
+from src.funciones import ecuacion_a, ecuacion_b, ecuacion_c, ecuacion_d
+from src.funciones import graficar
+
+
 file1 = open('./data/punto_2/pregunta.md').read()
 
 
@@ -8,5 +14,31 @@ def punto_dos():
     # -------------------------------------------------------------
     st.title("Punto 2 ")
     # -------------------------------------------------------------
+
+    # Importamos la clase MetodosNumericos que proporcionaste
+
+    # a) La solución numérica a la ecuación ex = 2(1 − x)
+    precision_vs_tiempo_a = tiempo_vs_precision(
+        ecuacion_a, MetodosNumericos.biseccion, 1e-8, 1e-15, 1e-8)
+    st.pyplot(graficar(precision_vs_tiempo_a,
+              'Tiempo vs Precisión para ex = 2(1 − x)'))
+
+    # b) Una aproximación numérica a la raíz de f(x) = x^3 + x − 7
+    precision_vs_tiempo_b = tiempo_vs_precision(
+        ecuacion_b, MetodosNumericos.newton, 1e-8, 1e-15, 1e-8)
+    st.pyplot(graficar(precision_vs_tiempo_b,
+              'Tiempo vs Precisión para x^3 + x − 7'))
+
+    # c) Halle la raíz positiva de la ecuación x = 5(1 − e^−x ) con cinco decimales de aproximación
+    precision_vs_tiempo_c = tiempo_vs_precision(
+        ecuacion_c, MetodosNumericos.newton, 1e-8, 1e-15, 1e-8)
+    st.pyplot(graficar(precision_vs_tiempo_c,
+              'Tiempo vs Precisión para x = 5(1 − e^−x )'))
+
+    # d) Halle la raíz de la función f(x) = x − 3e^−x con 6 decimales de aproximación
+    precision_vs_tiempo_d = tiempo_vs_precision(
+        ecuacion_d, MetodosNumericos.newton, 1e-8, 1e-15, 1e-8)
+    st.pyplot(graficar(precision_vs_tiempo_d,
+              'Tiempo vs Precisión para x − 3e^−x'))
 
     st.markdown(file1)
--- src/funciones.py
+++ src/funciones.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import base64
+from src.MetodosNumericos import MetodosNumericos
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 
 def displayPDF(file):
@@ -11,3 +15,57 @@ def displayPDF(file):
 
         # Displaying File
         st.markdown(pdf_display, unsafe_allow_html=True)
+
+# --------- Segundo punto -------------
+
+
+def tiempo_vs_precision(funcion, metodo, precision_inicial, precision_final, paso):
+    tiempos = []
+    precisiones = np.arange(precision_inicial, precision_final, paso)
+    for precision in precisiones:
+        start_time = time.time()
+        metodo(funcion, precision)
+        end_time = time.time()
+        tiempo = end_time - start_time
+        tiempos.append(tiempo)
+    return precisiones, tiempos
+
+
+def ecuacion_a(x, precision):
+    def f(x):
+        return np.exp(x) - 2*(1 - x)
+    metodos = MetodosNumericos(f=f)
+    return metodos.biseccion(0, 3, tol=precision)
+
+
+def ecuacion_b(x, precision):
+    def f(x):
+        return x**3 + x - 7
+    metodos = MetodosNumericos(f=f)
+    return metodos.newton(x, tol=precision)
+
+
+def ecuacion_c(x, precision):
+    def f(x):
+        return x - 5*(1 - np.exp(-x))
+    metodos = MetodosNumericos(f=f)
+    return metodos.newton(x, tol=precision)
+
+
+def ecuacion_d(x, precision):
+    def f(x):
+        return x - 3*np.exp(-x)
+    metodos = MetodosNumericos(f=f)
+    return metodos.newton(x, tol=precision)
+
+
+def graficar(tiempo_vs_precision, titulo):
+    precisiones, tiempos = tiempo_vs_precision
+    plt.plot(precisiones, tiempos, marker='o')
+    plt.title(titulo)
+    plt.xlabel('Precisión')
+    plt.ylabel('Tiempo (s)')
+    plt.grid(True)
+    return plt.gcf()
+
+# -------------------------------------
