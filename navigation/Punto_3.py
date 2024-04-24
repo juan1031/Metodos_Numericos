38f07750dbfaa20b524cddbd0895e56fe6e05117
juan1031 <jdbv2000@gmail.com>
2024-04-24 11:34:37 -0500

    update

--- navigation/Punto_3.py
+++ navigation/Punto_3.py
@@ -27,23 +27,23 @@ def punto_tres():
         st.markdown(
             '#### Gráfico y punto critico de la función $f(x) = e^{-x}-x$')
         st.plotly_chart(mn.plot_function([-5, 5]))
-
+        st.markdown('##### Condición de primer orden')
         st.markdown(file1)
 
     with col2:
         def f2(x): return np.log(x)-(5*x**2)
         def f2_prima(x): return (1/x)-10*x
         mn = MetodosNumericos(f=f2, f_prima=f2_prima)
-        x = mn.gss_max(0, 0.5)
+        x = mn.gss_max(0.1, 0.5)
         st.markdown(
             '#### Gráfico y punto critico de la función $f(x)=log(x)-5x^2$')
         chart = PlotlyChartPuntoC(
             f2, [-5, 5], x)
         chart.show()
-
+        st.markdown('##### Condición de primer orden')
         st.markdown(file2)
 
-        st.markdown('''Se pudo demostrar un punto critico en el dominio el cual es con la condición de primer orden el cual es
+        st.markdown('''Se pudo demostrar un punto critico en el dominio el cual es con la condición de primer orden la cual es
                     $\sqrt{1/10}$. ''' + f'''El punto critico para ésta función se encontro mediante el metodo numérico GSS (Golden-section search)
                     otorgando el valor de **{x}**, el detalle de la evaluación en el punto se da en la gráfica sin embargo tambien 
                     se lo describiremos más sencillo a continuación: **f({x}) = {f(x)}**''')
